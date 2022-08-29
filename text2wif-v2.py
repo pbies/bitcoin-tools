af5a04dfa0f2fe89e55a65f8afcc7a393a781c58
Piotr Biesiada <39916932+pbies@users.noreply.github.com>
2022-08-29 20:59:57 +0200

    Some updates

--- text2wif-v2.py
+++ text2wif-v2.py
@@ -3,15 +3,17 @@
 import base58
 import hashlib
 import sys
+from tqdm import tqdm
+
+outfile = open("output.txt","w")
 
 with open("input.txt","r",encoding="utf-8") as f:
 	content = f.readlines()
 
 content = [x.rstrip('\n') for x in content]
 
-o = open('output.txt','w',encoding="utf-8")
-
-for line in content:
+for l in tqdm(content, total=len(content), unit=" lines"):
+	line=l.rstrip('\n')
 	a=line.encode('utf-8')
 	b=hashlib.sha256(a).digest()
 	c=b.hex()
@@ -19,4 +21,6 @@ for line in content:
 	f=bytes.fromhex(d)
 	g=base58.b58encode_check(f)
 	h=g.decode('utf-8')+" 0 # "+line+"\n"
-	o.write(h)
+	outfile.write(h)
+
+outfile.close()
