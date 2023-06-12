370dfc656c0b2825f88a860638754ee2f61af576
Piotr Biesiada <pmbiesiada@gmail.com>
2023-06-12 04:16:36 +0200

    Improve

---
+++ many-addresses-balance.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import requests
+
+i=open('input.txt')
+
+for addr in i:
+	addr=addr.strip()
+	r=requests.get('https://blockchain.info/q/addressbalance/'+addr)
+
+	if not r.status_code==200:
+		print('Error',r.status_code)
+		exit()
+
+	b=int(r.text)
+
+	print(addr,'\t',r.text,'sat\t',b/100000,'mBTC\t',b/100000000,'BTC')
