657e0590664a9c7c01fc4413595daf92875f140c
Piotr Biesiada <pmbiesiada@gmail.com>
2024-05-16 10:48:41 +0200

    2 new files

---
+++ mnemos2wifs-addrs-v2.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+# sudo apt install python3-pip
+# pip3 install hdwallet
+
+from hdwallet import HDWallet
+from hdwallet.symbols import BTC
+from pprint import pprint
+from tqdm import tqdm
+from tqdm.contrib.concurrent import process_map
+import pprint
+import random
+
+hdwallet = HDWallet(symbol=BTC)
+
+def go(k):
+	try:
+		hdwallet.from_mnemonic(k)
+	except:
+		return
+	outfile.write(hdwallet.wif()+'\n')
+	outfile.write(hdwallet.p2pkh_address()+'\n')
+	outfile.write(hdwallet.p2sh_address()+'\n')
+	outfile.write(hdwallet.p2wpkh_address()+'\n')
+	outfile.write(hdwallet.p2wpkh_in_p2sh_address()+'\n')
+	outfile.write(hdwallet.p2wsh_address()+'\n')
+	outfile.write(hdwallet.p2wsh_in_p2sh_address()+'\n')
+
+infile = open('input.txt','r')
+outfile = open('output.txt','w')
+
+lines = infile.readlines()
+lines = [x.strip() for x in lines]
+
+process_map(go, lines, max_workers=12, chunksize=10000)
+
+import sys
+print('\a',end='',file=sys.stderr)
