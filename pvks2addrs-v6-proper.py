dbc1d01d1bf847ca2db6e69e9c7657d925e9583a
pbies <pmbiesiada@gmail.com>
2024-12-01 13:52:20 +0100

    small fix

--- pvks2addrs-v6-proper.py
+++ pvks2addrs-v6-proper.py
@@ -12,14 +12,14 @@ import sys, base58
 hdwallet = HDWallet(symbol=BTC)
 
 def pvk_to_wif2(key_hex):
-	return base58.b58encode_check(b'\x80' + bytes.fromhex(key_hex))
+	return base58.b58encode_check(b'\x80' + bytes.fromhex(key_hex)).decode()
 
 def go(k):
 	try:
 		hdwallet.from_private_key(private_key=k)
 	except:
 		return
-	wif=pvk_to_wif2(k).decode()
+	wif=pvk_to_wif2(k)
 	a=wif+'\n'+hdwallet.wif()+'\n'+hdwallet.p2pkh_address()+'\n'+hdwallet.p2sh_address()+'\n'+hdwallet.p2wpkh_address()+'\n'+hdwallet.p2wpkh_in_p2sh_address()+'\n'+hdwallet.p2wsh_address()+'\n'+hdwallet.p2wsh_in_p2sh_address()+'\n\n'
 	outfile.write(a)
 	outfile.flush()
