35f0459471e01f3580d9b50f5b85de9f5de9f585
pbies <pmbiesiada@gmail.com>
2024-12-17 05:52:14 +0100

    new files, one fix

---
+++ eth/mnemos2addrs-bal-v6-mt.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import mnemonic
+import binascii
+from web3.auto import w3
+from web3 import Web3
+from tqdm import tqdm
+from tqdm.contrib.concurrent import process_map
+
+i = open('input.txt','r').read().splitlines()
+
+o = open('output.txt','a')
+
+alchemy_url = 'https://worldchain-mainnet.g.alchemy.com/v2/vyNyoamdEv_67zitim9G09wuPVUSKkLG'
+w3 = Web3(Web3.HTTPProvider(alchemy_url))
+w3.eth.account.enable_unaudited_hdwallet_features()
+
+def go(i):
+	try:
+		acc = w3.eth.account.from_mnemonic(i)
+	except:
+		return
+	address = w3.to_checksum_address(acc.address)
+	bal=w3.eth.get_balance(address,'latest')
+	h=acc._private_key.hex()
+	b='{0:.18f}'.format(bal/1e18)
+	o.write(address+' = '+str(b)+' ETH = '+i+' = '+h+'\n')
+	o.flush()
+	if bal>=1e15:
+		print('\n\a'+address+' = '+str(b)+' ETH = '+i+' = '+h, flush=True)
+
+process_map(go, i, max_workers=4, chunksize=1000)
+
+import sys
+print('\a', end='', file=sys.stderr)
---
+++ pvks2addrs-v7-new-hdw.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+# sudo apt install python3-pip
+# pip3 install hdwallet
+
+from hdwallet import HDWallet
+from hdwallet.cryptocurrencies import Bitcoin as BTC
+from hdwallet.hds import BIP32HD
+#from hdwallet.symbols import BTC
+from hdwallet.mnemonics import BIP39Mnemonic
+from tqdm import tqdm
+from tqdm.contrib.concurrent import process_map
+import sys, base58
+
+def pvk_to_wif2(key_hex):
+	return base58.b58encode_check(b'\x80' + bytes.fromhex(key_hex)).decode()
+
+def go(k):
+	#try:
+	hdwallet = HDWallet(cryptocurrency=BTC, hd=BIP32HD).from_private_key(private_key=k)
+	#except:
+	#	return
+	wif=pvk_to_wif2(k)
+	a=wif+'\n'+hdwallet.wif()+'\n'+hdwallet.address("P2PKH")+'\n'+hdwallet.address("P2SH")+'\n'+hdwallet.address("P2TR")+'\n'+hdwallet.address("P2WPKH")+'\n'+hdwallet.address("P2WPKH-In-P2SH")+'\n'+hdwallet.address("P2WSH")+'\n'+hdwallet.address("P2WSH-In-P2SH")+'\n\n'
+	outfile.write(a)
+	outfile.flush()
+
+print('Reading...', flush=True)
+infile = open('input.txt','r')
+lines = infile.read().splitlines()
+
+print('Writing...', flush=True)
+outfile = open('output.txt','w')
+process_map(go, lines, max_workers=16, chunksize=1000)
+
+print('\a', end='', file=sys.stderr)
