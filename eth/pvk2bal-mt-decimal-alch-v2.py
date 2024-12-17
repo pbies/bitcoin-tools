f672debfe688bd99ae66582a7bb8930bf5ed22be
pbies <pmbiesiada@gmail.com>
2024-12-17 16:35:10 +0100

    new scripts

---
+++ eth/pvk2bal-mt-decimal-alch-v2.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+from Crypto.Hash import keccak
+from ecpy.curves import Curve
+from eth_keys import keys
+from eth_utils import to_checksum_address
+from subprocess import check_output
+from tqdm import tqdm
+from tqdm.contrib.concurrent import process_map
+from web3 import Web3
+import base58
+import hashlib
+import json
+import os
+import requests
+import sys
+
+workers=4
+
+url = 'https://eth-mainnet.g.alchemy.com/v2/vyNyoamdEv_67zitim9G09wuPVUSKkLG'
+w3 = Web3(Web3.HTTPProvider(url))
+
+headers = {'content-type': 'application/json'}
+
+def go(l):
+	private_key = int(l,16)
+
+	cv     = Curve.get_curve('secp256k1')
+	pu_key = private_key * cv.generator # just multiplying the private key by generator point (EC multiplication)
+
+	concat_x_y = pu_key.x.to_bytes(32, byteorder='big') + pu_key.y.to_bytes(32, byteorder='big')
+	k=keccak.new(digest_bits=256)
+	k.update(concat_x_y)
+	eth_addr = '0x' + k.hexdigest()[-40:]
+
+	cksum=Web3.to_checksum_address(eth_addr)
+	payload = {
+		'jsonrpc': '2.0',
+		'method': 'eth_getBalance',
+		'params': [cksum,'latest'],
+		'id': 1
+	}
+	try:
+		response = requests.post(url, data=json.dumps(payload), headers=headers).json()
+	except:
+		e=open('errors.txt','a')
+		e.write(cksum+'\n')
+		e.flush()
+		e.close()
+		return
+	try:
+		bal=int(response['result'],16)
+		b='{0:.18f}'.format(bal/1e18)
+		o.write(l+' '+cksum+' '+b+'\n')
+	except:
+		pass
+	o.flush()
+
+print('Reading...')
+i=open('input.txt','r').read().splitlines()
+
+print('Writing...')
+o=open('output.txt','w')
+process_map(go, i, max_workers=workers, chunksize=100)
+#for x in tqdm(i):
+#	go(x)
+
+o.close()
+
+print('Done!\a', file=sys.stderr)
---
+++ repair-wallet-v1.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+from bsddb3 import db
+import json
+import os
+import sys
+
+if len(sys.argv) != 2:
+	print("Usage: python dump_wallet.py <wallet_file_path>")
+	sys.exit(1)
+
+wallet_file_path = sys.argv[1]
+
+data=[]
+
+try:
+	wallet_db = db.DB()
+	wallet_db.open(wallet_file_path, 'main', db.DB_BTREE, db.DB_RDONLY)
+	cursor = wallet_db.cursor()
+	data = {}
+
+	while True:
+		record = cursor.next()
+		if record is None:
+			break
+
+		key, value = record
+
+		data[key] = value
+
+	cursor.close()
+	wallet_db.close()
+
+except Exception as e:
+	print(f"Failed to load wallet file: {e}")
+	sys.exit(1)
+
+wallet_db2 = db.DB()
+fn, ext = os.path.splitext(wallet_file_path)
+wallet_db2.open(fn+'-repaired'+ext, 'main', db.DB_BTREE, db.DB_CREATE)
+
+for k, v in data.items():
+	wallet_db2.put(k, v)
+
+print('\a', end='', file=sys.stderr)
