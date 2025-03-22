b2d4c85a57403916fc176a5351e8d57e2e7c0935
pbies <pmbiesiada@gmail.com>
2025-03-22 03:17:55 +0100

    Create validate-ents-v2-re.py

---
+++ validate-ents-v2-re.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+from tqdm import tqdm
+from tqdm.contrib.concurrent import process_map
+import sys
+import time
+import re
+
+infile = open('input.txt','r')
+good = open('good.txt','w')
+bad = open('bad.txt','w')
+
+def go(k):
+	l=len(k)
+	if l<32 or not re.search('^[0-9A-Fa-f]+$', k):
+		bad.write(k+'\n')
+		bad.flush()
+	if l==32:
+		good.write(f'{k}\n')
+		good.flush()
+	if l>32:
+		good.write(f'{k[:32]}\n{k[-32:]}\n')
+		good.flush()
+
+print('Reading...', flush=True)
+lines = infile.read().splitlines()
+
+print('Writing...', flush=True)
+for line in tqdm(lines):
+	go(line)
+
+print('\a', end='', file=sys.stderr)
