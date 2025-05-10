916a72c28e8610bacf1482b534cd6d3efa8d347e
pbies <pmbiesiada@gmail.com>
2025-05-11 00:01:55 +0200

    important fixes

--- pvks2addrs-v9-new-hdw-pool-read.py
+++ pvks2addrs-v9-new-hdw-pool-read.py
@@ -10,12 +10,13 @@ from hdwallet.mnemonics import BIP39Mnemonic
 from multiprocessing import Pool
 from tqdm import tqdm
 from tqdm.contrib.concurrent import process_map
-import sys, base58
+import sys, base58, os
 
 def pvk_to_wif2(key_hex):
 	return base58.b58encode_check(b'\x80' + bytes.fromhex(key_hex)).decode()
 
 def go(k):
+	k=k.decode()
 	try:
 		hdwallet = HDWallet(cryptocurrency=BTC, hd=BIP32HD).from_private_key(private_key=k)
 	except:
@@ -24,22 +25,22 @@ def go(k):
 	a=wif+'\n'+hdwallet.wif()+'\n'+hdwallet.address("P2PKH")+'\n'+hdwallet.address("P2SH")+'\n'+hdwallet.address("P2TR")+'\n'+hdwallet.address("P2WPKH")+'\n'+hdwallet.address("P2WPKH-In-P2SH")+'\n'+hdwallet.address("P2WSH")+'\n'+hdwallet.address("P2WSH-In-P2SH")+'\n\n'
 	outfile.write(a)
 
-print('Reading...', flush=True)
-infile = open('input.txt','r')
-lines = infile.read().splitlines()
+infile = open('input.txt','rb')
+size = os.path.getsize('input.txt')
+outfile = open('output.txt','w')
 
 th=24
-max_=len(lines)
+cnt=1e6
+tmp=0
 
-print('Writing...', flush=True)
-outfile = open('output.txt','w')
-i=0
-with Pool(processes=th) as p, tqdm(total=max_) as pbar:
-	for result in p.imap_unordered(go, lines, chunksize=1000):
-		if i%1000==0:
+with Pool(processes=th) as p, tqdm(total=size, unit='B', unit_scale=True) as pbar:
+	for result in p.imap_unordered(go, infile, chunksize=1000):
+		pos=infile.tell()
+		r=pos-tmp
+		if r>cnt:
 			outfile.flush()
-			pbar.update(1000)
+			tmp=pos
+			pbar.update(r)
 			pbar.refresh()
-		i=i+1
 
 print('\a', end='', file=sys.stderr)
