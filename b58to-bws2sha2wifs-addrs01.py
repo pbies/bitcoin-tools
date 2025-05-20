3baca27d46d93d90b90fcdd150e847fc44705470
pbies <pmbiesiada@gmail.com>
2025-05-21 01:28:27 +0200

    new scripts

---
+++ b58to-bws2sha2wifs-addrs01.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+# sudo apt install python3-pip
+# pip3 install hdwallet
+
+from hdwallet import HDWallet
+from hdwallet.cryptocurrencies import Bitcoin as BTC
+from hdwallet.hds import BIP32HD
+from hdwallet.mnemonics import BIP39Mnemonic
+from multiprocessing import Pool
+from tqdm import tqdm
+from tqdm.contrib.concurrent import process_map
+import sys, base58, os, hashlib
+
+def pvk_to_wif2(key_hex):
+	return base58.b58encode_check(b'\x80' + bytes.fromhex(key_hex)).decode()
+
+infile = open('input.txt','rb')
+size = os.path.getsize('input.txt')
+outfile = open('output.txt','w')
+
+def go(k):
+	k=k.decode()[:-1]
+	try:
+		l=base58.b58decode_check(k)
+		m=hashlib.sha256(l).hexdigest()
+		hdwallet = HDWallet(cryptocurrency=BTC, hd=BIP32HD).from_private_key(private_key=m)
+	except:
+		return
+	wif=pvk_to_wif2(m)
+	a=wif+'\n'+hdwallet.wif()+'\n'+hdwallet.address("P2PKH")+'\n'+hdwallet.address("P2SH")+'\n'+hdwallet.address("P2TR")+'\n'+hdwallet.address("P2WPKH")+'\n'+hdwallet.address("P2WPKH-In-P2SH")+'\n'+hdwallet.address("P2WSH")+'\n'+hdwallet.address("P2WSH-In-P2SH")+'\n\n'
+	outfile.write(a)
+
+th=24
+cnt=1e6
+tmp=0
+
+with Pool(processes=th) as p, tqdm(total=size, unit='B', unit_scale=True) as pbar:
+	for result in p.imap_unordered(go, infile, chunksize=1000):
+		pos=infile.tell()
+		r=pos-tmp
+		if r>cnt:
+			outfile.flush()
+			tmp=pos
+			pbar.update(r)
+			pbar.refresh()
+
+print('\a', end='', file=sys.stderr)
