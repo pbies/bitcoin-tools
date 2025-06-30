bc5ffd02ed1e66ccae739c9be3dfbd67e791b92a
pbies <pmbiesiada@gmail.com>
2025-06-30 18:49:01 +0200

    important fix

--- strip-file.py
+++ strip-file.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 
-import sys
+import sys, os
 from tqdm import tqdm
 
 print('Reading...', flush=True)
-i=open(sys.argv[1],'rb').read().splitlines()
-o=open(sys.argv[1]+'.out','wb')
+i=open(sys.argv[1], 'rb').read().splitlines()
+o=open(sys.argv[1]+'.out', 'wb')
 
 print('Writing...', flush=True)
 for x in tqdm(i):
 	y=x.strip()
 	o.write(y+b'\n')
 	o.flush()
+
+os.rename(sys.argv[1]+'.out', sys.argv[1], 'rb')
 
 print('\a', end='', file=sys.stderr)
