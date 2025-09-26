15c0554d30236f1720b53ad0c525be42ee80e593
pbies <pmbiesiada@gmail.com>
2025-09-27 00:46:28 +0200

    Create addrs2bal-mt-final-v9-read.py

---
+++ eth/addrs2bal-mt-final-v9-read.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+from tqdm import tqdm
+from tqdm.contrib.concurrent import process_map
+from web3 import Web3
+from web3.auto import w3
+import binascii
+import mnemonic
+import os, sys
+from multiprocessing import Pool
+
+alchemy_url = 'https://eth-mainnet.g.alchemy.com/v2/API_KEY'
+w3 = Web3(Web3.HTTPProvider(alchemy_url))
+w3.eth.account.enable_unaudited_hdwallet_features()
+
+def go(i):
+	i=i.decode()[:-1]
+	address = w3.to_checksum_address(i[-42:])
+	#pvk=i[43:]
+	try:
+		bal=w3.eth.get_balance(address,'latest')
+	except:
+		try:
+			bal=w3.eth.get_balance(address,'latest')
+		except:
+			with open('errors.txt','a') as e:
+				e.write(f'{i}\n')
+			return
+	b='{0:.18f}'.format(bal/1e18)
+	with open('output.txt','a') as o:
+		o.write(f'{str(b)} ETH {i}\n')
+	if bal>=int(1e15):
+		print(f'\n\a{i} {str(b)} ETH', flush=True)
+
+def main():
+	os.system('cls||clear')
+	size = os.path.getsize('input.txt')
+	i = open('input.txt','rb')
+	if os.path.exists("output.txt"):
+		os.remove("output.txt")
+	if os.path.exists("errors.txt"):
+		os.remove("errors.txt")
+	t=i.tell()
+	tmp = 0
+	c=0
+	cnt=1000
+	th=4
+	with Pool(processes=th) as p, tqdm(total=size, unit='B', unit_scale=True) as pbar:
+		for result in p.imap_unordered(go, i, chunksize=10):
+			t=i.tell()
+			r=t-tmp
+			if r>cnt:
+				tmp=t
+				pbar.update(r)
+				pbar.refresh()
+
+	print('\a', end='', file=sys.stderr)
+
+if __name__=='__main__':
+	main()
