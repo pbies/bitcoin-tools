b7d6e51434579ad40b81ff4395487cd2683dddaa
pbies <pmbiesiada@gmail.com>
2025-09-30 07:27:05 +0200

    tidy templates

--- templates/template05.py
+++ templates/template05.py
@@ -4,12 +4,14 @@ import sys, os, datetime, time
 
 def main():
 	os.system('cls' if os.name == 'nt' else 'clear')
-	print('Program started: '+datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+	start_msg='Program started: '+datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+	print(start_msg)
 	start_time=time.time()
 
 	# your code
 
 	stop_time=time.time()
+	print(start_msg)
 	print('Program stopped: '+datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 	print(f'Execution took: {str(datetime.timedelta(seconds=stop_time-start_time))}')
 	print('\a', end='', file=sys.stderr)
